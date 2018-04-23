088a82e007474f1a605ab2efb664693de6b64032
Guillaume Noireaux <gnoireaux@gmail.com>
2018-04-23 18:31:59 +0200

    refact: style

--- spec/trivial_chat_spec.rb
+++ spec/trivial_chat_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe TrivialChat do
-  it "has a version number" do
+  it 'has a version number' do
     expect(TrivialChat::VERSION).not_to be nil
   end
 
-  context "The server is running." do 
+  context 'The server is running.' do
     before do
       @port = 4242
       @cs = TrivialChatServer.new(@port)
@@ -18,13 +18,13 @@ RSpec.describe TrivialChat do
       TCPSocket.new 'localhost', @port
     end
 
-    it "accepts 3 (multiple!) connections" do
-      3.times do 
+    it 'accepts 3 (multiple!) connections' do
+      3.times do
         expect(new_client).to be_a TCPSocket
       end
     end
 
-    it "receives messages from a socket" do
+    it 'receives messages from a socket' do
       new_client
     end
 

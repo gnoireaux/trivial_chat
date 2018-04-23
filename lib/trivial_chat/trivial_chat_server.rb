17429e2c770c544a449a588d8d5c3b5f674c3db9
Guillaume Noireaux <gnoireaux@gmail.com>
2018-04-23 21:24:05 +0200

    feat: it broadcasts!

--- lib/trivial_chat/trivial_chat_server.rb
+++ lib/trivial_chat/trivial_chat_server.rb
@@ -4,15 +4,32 @@ class TrivialChatServer
     @port = port
     @server_socket = TCPServer.new port
     @thread_accept = nil
+    @clients = []
   end
 
   def run
     @thread_accept = Thread.new do
       loop do
-        @server_socket.accept_nonblock(exception: false)
-        sleep 0.01
+        if ready = IO.select([@server_socket, @clients].flatten!) then
+          ready.first.each do |ready_for_reading|
+            if ready_for_reading == @server_socket then
+              new_client
+            else
+              msg = ready_for_reading.gets
+              @clients.each do |client|
+                client.puts msg
+              end
+            end
+          end
+        end
       end
     end
+  end
+
+  def new_client
+    nc = @server_socket.accept
+    @clients << nc
+    nc.puts 'Connected to server' 
   end
 
   def stop

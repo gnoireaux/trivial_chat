7b40b90258135b167ee06776bc51fa9aa18c2fa0
Guillaume Noireaux <gnoireaux@gmail.com>
2018-04-23 17:58:20 +0200

    refact: tests

--- spec/trivial_chat_spec.rb
+++ spec/trivial_chat_spec.rb
@@ -3,22 +3,27 @@ RSpec.describe TrivialChat do
     expect(TrivialChat::VERSION).not_to be nil
   end
 
-  it "accepts 3 (multiple!) connections" do
-    port = 4242
-    cs = TrivialChatServer.new(port)
-    cs.run
-    client1 = TCPSocket.new 'localhost', port
-    expect(client1).to be_a TCPSocket
-    expect(TCPSocket.new 'localhost', port).to be_a TCPSocket
-    expect(TCPSocket.new 'localhost', port).to be_a TCPSocket
+  context "The server is running." do 
+    before do
+      @port = 4242
+      @cs = TrivialChatServer.new(@port)
+      @cs.run
+    end
+
+    it "accepts 3 (multiple!) connections" do
+      client1 = TCPSocket.new 'localhost', @port
+      expect(client1).to be_a TCPSocket
+      expect(TCPSocket.new 'localhost', @port).to be_a TCPSocket
+      expect(TCPSocket.new 'localhost', @port).to be_a TCPSocket
+    end
+
+    # it "receives messages from a socket" do
+    # end
+
+    # it "broadcasts messages to all clients" do
+    # end
+
+    # it "should handle network failure" do
+    # end
   end
-
-  # it "receives messages from a socket" do
-  # end
-  
-  # it "broadcasts messages to all clients" do
-  # end
-
-  # it "should handle network failure" do
-  # end
 end

55d18a279feb2867928d59648735ce92392244e8
Guillaume Noireaux <gnoireaux@gmail.com>
2018-04-30 11:16:56 +0200

    fix: require client

--- lib/trivial_chat.rb
+++ lib/trivial_chat.rb
@@ -1,6 +1,7 @@
 require "trivial_chat/version"
 require 'socket'
 require 'trivial_chat/trivial_chat_server'
+require 'trivial_chat/trivial_chat_client'
 
 module TrivialChat
   # Your code goes here...

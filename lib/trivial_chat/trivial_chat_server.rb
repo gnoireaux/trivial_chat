c57a79fb10a4a552af1307fdf509a1d6060c4639
Guillaume Noireaux <gnoireaux@gmail.com>
2018-05-15 16:23:00 +0200

    refact: style

--- lib/trivial_chat/trivial_chat_server.rb
+++ lib/trivial_chat/trivial_chat_server.rb
@@ -10,20 +10,19 @@ class TrivialChatServer
   def run
     @thread_accept = Thread.new do
       loop do
-        if ready = IO.select([@server_socket, @clients].flatten!)
-          ready.first.each do |ready_for_reading|
-            if ready_for_reading == @server_socket
-              accept_new_client
-            else
-              broadcast_message_from ready_for_reading
-            end
+        next unless ready = IO.select([@server_socket, @clients].flatten)
+        ready.first.each do |ready_for_reading|
+          if ready_for_reading == @server_socket
+            accept_new_client
+          else
+            broadcast_message_from ready_for_reading
           end
         end
       end
     end
   end
   alias start run
-  
+
   def broadcast_message_from(ready_for_reading)
     msg = ready_for_reading.gets
     @clients.each do |client|
